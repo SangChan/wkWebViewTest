dbe0640eaa3403e660f82a7ec4e2c113efc92dc3
SangChan Lee <sangchan82@gmail.com>
2014-09-01 10:59:00 +0900

    Initial Commit

---
+++ wkWebViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wkWebViewTest
+//
+//  Created by SangChan on 2014. 9. 1..
+//  Copyright (c) 2014년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wkWebViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wkWebViewTest
+//
+//  Created by SangChan on 2014. 9. 1..
+//  Copyright (c) 2014년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
